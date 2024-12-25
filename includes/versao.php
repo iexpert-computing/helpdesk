c8149ca23f2b42159cb6eea1703737b1b1323ec7
Eloilson <eloilsonrocha@gmail.com>
2024-12-25 18:00:51 -0300

    customizando nome logos e icones

--- includes/versao.php
+++ includes/versao.php
@@ -1,8 +1,8 @@
 <?php 
 //Versao do Sistema
 define ( "VERSAO", "6.0" );
-define ("COMPANY", "OcoMon");
-define ("COMPANY_URL", "https://ocomon.com.br");
-define ("APP_NAME", "OcoMon");
-define ("APP_URL", "https://ocomon.com.br");
+define ("COMPANY", "Helpdesk");
+define ("COMPANY_URL", "https://iexpert.net.br");
+define ("APP_NAME", "Helpdesk");
+define ("APP_URL", "https://iexpert.net.br");
 ?>
